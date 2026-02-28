45e133ba1d648ebbc5761077698aa008b49a0228
Shubhankar Kumar <shubhankarpandey2007@outlook.com>
2026-02-28 13:49:48 +0530

    Stable Version again

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ import img_catcher
 import recogniser
 import os
 
+def student_info(data):
+    registered_new = False
+                
+    while True:
+        print("\nStudent Registration")
+        name = input("Enter Student Name (or 'q' to quit): ").strip().replace(" ","_")
+        if name.lower() == 'q': 
+            break
+        roll_no = input("Enter Roll Number: ").strip()
+        dataHandler.save_student_info(name, roll_no)
+        img_catcher.faceCreator(data['path'], name=name)
+                    
+        registered_new = True
+        print(f"Captured images for {name}.")
+
+    if registered_new:
+                    print("Updating AI memory for all new registrations...")
+                    dataHandler.refresh_db(data['path'])
+
 def main():
     print("System Started...")
     data = dataHandler.loading()
@@ -14,18 +33,16 @@ def main():
         print("No students are registered. Do you want to register first?", end=" ")
         choice = input().lower()
         if choice == 'yes' or choice == 'y':
-            img_catcher.faceCreator(data['path'])
-            recogniser.analyzing(data['path'])
+            student_info(data)
     
     while True:
         print("1. Registering \n2. Attending \n3. Exit")
         choice = int(input("Enter your choice: "))
 
         if choice == 1:
-            checker = int(input("Enter code: "))
+            checker = input("Enter code: ") 
             if dataHandler.hash_pw(checker) == data['code']:
-                img_catcher.faceCreator(data['path'])
-                dataHandler.refresh_db(data['path'])
+                student_info(data)
         
         elif choice == 2:
             register = recogniser.recognise(data['path'],img_catcher.capture(data['folder']))
--- recogniser.py
+++ recogniser.py
@@ -1,5 +1,5 @@
 import os
-import warnings, logging
+import warnings, logging,json
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
@@ -7,8 +7,29 @@ warnings.filterwarnings('ignore')
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
 # Above code is there to remove the warnings and loggings made by Tensorflow and DeepFace. Don't remove it
+
 from deepface import DeepFace
 import pandas as pd
+import csv, datetime
+
+def logging_attendance(img_path):
+    name = os.path.basename(img_path)
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    with open("attendance_log.csv", mode='r',newline='') as f:
+        if f"{name},{today}" in f.read():
+                print(f"{name} is already marked present for today.")
+                return
+
+    with open("attendance_log.csv", mode='a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([
+            name, 
+            datetime.datetime.now().strftime('%Y-%m-%d'), 
+            datetime.datetime.now().strftime('%H:%M:%S')
+        ])
+
+    print(f"Attendance recorded in CSV for {name}")
 
 def analyzing(db_path):
     print("Scanning database to update memory...")
@@ -40,8 +61,20 @@ def recognise(db_path, img_path):
         distance = row['distance']
 
         if distance < 0.55: 
-            print(f"Match found! {row['identity']} (Dist: {distance:.2f})")
-            return row['identity']
+            matched_name = os.path.basename(row['identity']).split('.')[0]
+            
+            roll_no = None
+            if os.path.exists("student.json"):
+                with open("student.json", "r") as f:
+                    students = json.load(f)
+                    for s in students:
+                        if s['name'] == matched_name:
+                            roll_no = s['roll_no']
+                            break
+
+            print(f"Match found! Name: {matched_name} | Roll: {roll_no}")
+            logging_attendance(row['identity']) 
+            return matched_name
         else:
             print(f"Person looks familiar (Dist: {distance:.2f}), but not sure enough.")
 
